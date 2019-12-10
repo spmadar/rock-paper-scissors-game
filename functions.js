3d9ce4e074e44b861d2a3ede5f3230d273899bf4
Sophia Madar <spmadar@gmail.com>
2019-12-10 14:32:51 -0500

    added images, fixed errors

--- functions.js
+++ functions.js
@@ -8,6 +8,7 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+
 function getComputerChoice () {
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
